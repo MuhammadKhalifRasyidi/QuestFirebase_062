864ac3725410bf43f8b43586b8b1b6d98db82c30
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-01-12 15:18:59 +0700

    menambahkan fungsi insertMahasiswa untuk menyimpan data ke Firestore

--- app/src/main/java/com/example/meet14/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/meet14/repository/NetworkMahasiswaRepository.kt
@@ -6,41 +6,47 @@ import com.google.firebase.firestore.Query
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
-class NetworkMahasiswaRepository (
+class NetworkMahasiswaRepository(
     private val firestore: FirebaseFirestore
 ) : MahasiswaRepository {
+
     override suspend fun getMahasiswa(): Flow<List<Mahasiswa>> = callbackFlow {
 
         val mhsCollection = firestore.collection("Mahasiswa")
             .orderBy("nim", Query.Direction.ASCENDING)
             .addSnapshotListener { value, error ->
 
-                if ( value != null) {
+                if (value != null) {
                     val mhsList = value.documents.mapNotNull {
                         it.toObject(Mahasiswa::class.java)!!
                     }
                     trySend(mhsList) // try send memberikan fungsi untuk mengirim data ke flow
                 }
             }
-        awaitClose{
+        awaitClose {
             mhsCollection.remove()
         }
     }
 
     override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa").add(mahasiswa).await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menambahkan data mahasiswa: ${e.message}")
+        }
     }
 
     override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+
     }
 
     override suspend fun deleteMahasiswa(nim: String) {
-        TODO("Not yet implemented")
+
     }
 
-    override suspend fun getMahasiswaByNim(nim: String): Flow<Mahasiswa> {
-        TODO("Not yet implemented")
+    override suspend fun getMahasiswaByNim(nim: String): Flow<Mahasiswa> = callbackFlow {
+
     }
 }
